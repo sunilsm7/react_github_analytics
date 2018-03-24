48bcf1cbd73cfb392de210d37b7be272e5ed48af
Sunil <sunil9766@gmail.com>
2018-03-24 11:52:05 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">GitHub Analytics</h1>
+        </header>
+        <p className="App-intro">
+          Watch this space....
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;

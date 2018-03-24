5507e8feddf390797fe52c0d5982310c736861e4
Sunil <sunil9766@gmail.com>
2018-03-24 12:12:57 +0530

    get json data from github

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
+import Button from './components/Button.jsx';
+import axios from 'axios';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username : 'No username',
+      info: ''
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  // handle button clicks
+  handleClick(e){
+    axios.get('https://api.github.com/users/sunilsm7')
+    .then(response => this.setState({
+      username: response.data.login,
+      info: JSON.stringify(response.data, undefined, 2)
+    }));
+  }
   render() {
     return (
       <div className="App">
@@ -10,6 +29,11 @@ class App extends Component {
         <p className="App-intro">
           Watch this space....
         </p>
+        <Button handleClick={this.handleClick} />
+        <p><b>Username:</b></p>
+        <p>{this.state.username}</p>
+        <b>Information:</b>
+        <pre>{this.state.info}</pre>
       </div>
     );
   }
